5471a4e91cf3d6beb54eeffc0f396c38ab5bdf2e
Raphael <ralfdu31@hotmail.fr>
2014-02-18 11:34:39 +0100

    Début du parser xml + librairies nécessaires + fichiers java mis à jour

--- src/ise/Algorithm.java
+++ src/ise/Algorithm.java
@@ -13,4 +13,44 @@ public class Algorithm {
 	public Node lastNodeVisitedByJonI(Path I, Path J) {
 		return null;
 	}
+	
+	public Flow firstFlowVisitedByJonI(Path I, Path J) {
+		return null;
+	}
+	
+	public Flow lastFlowVisitedByJonI(Path I, Path J) {
+		return null;
+	}
+	
+	public int minTimeTakenFromSourceToH(Flow f, Node h) {
+		return 0;
+	}
+	
+	public int maxTimeTakenFromSourceToH(Flow f, Node h) {
+		return 0;
+	}
+	
+	Node slowestNodeVisitedByJonI(Flow i, Flow j) {
+		return null;
+	}
+	
+	int computeM(Flow i, Node h) {
+		return 0;
+	}
+	
+	int computeA(Flow i, Flow j) {
+		return 0;
+	}
+	
+	int computeBetaSlow(Flow i) {
+		return 0;
+	}
+	
+	int computeDelta(Flow i, Node h) {
+		return 0;
+	}
+	
+	int computeW(Flow i, int t) {
+		return 0;
+	}
 }
