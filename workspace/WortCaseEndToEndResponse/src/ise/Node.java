f9779c3514930edf9be64d1b3165b731c6bf3c64
Raphael <ralfdu31@hotmail.fr>
2014-02-18 10:41:45 +0100

    Mise à jour des classes du package ise

--- workspace/WortCaseEndToEndResponse/src/ise/Node.java
+++ workspace/WortCaseEndToEndResponse/src/ise/Node.java
@@ -1,5 +1,19 @@
 package ise;
 
-public class Node {
+import java.util.HashMap;
 
+public class Node {
+	private HashMap<Flow, Integer> capacity;
+
+	public Node(HashMap<Flow, Integer> capacity) {
+		super();
+		this.capacity = capacity;
+	}
+	public HashMap<Flow, Integer> getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(HashMap<Flow, Integer> capacity) {
+		this.capacity = capacity;
+	}
 }
