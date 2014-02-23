a22599ed1ce86ea36119dbf9c48b9c0fc4fe5b77
Lucas Simonet <lucas.simonet@gmail.com>
2014-02-23 14:06:22 +0100

    Ecriture de la methode init dans Network.java permettant l'initialisation de chacune des variables hpf, spf, lpf de chacun des objets Flow contenu dans Network

--- src/ise/Network.java
+++ src/ise/Network.java
@@ -14,9 +14,7 @@ public class Network {
 		flows = new ArrayList<Flow>();
 		nodes = new ArrayList<Node>();
 	}
-	public void addFlow(Flow flow) {
-		flows.add(flow);
-	}
+	
 	public Network(List<Flow> flows, List<Node> nodes, int lmax, int lmin) {
 		super();
 		this.flows = flows;
@@ -24,27 +22,64 @@ public class Network {
 		this.lmax = lmax;
 		this.lmin = lmin;
 	}
+	
+	/**
+	 * Initialisation pour chaque Flow de leurs variables hpf, spf et lpf
+	 */
+	public void init(){
+		int i = 0, j = 0;
+		Flow current_flow = null, compared_flow = null;
+		for(i = 0; i < flows.size(); i++){
+			List<Flow> hpf = new ArrayList<Flow>();
+			List<Flow> spf = new ArrayList<Flow>();
+			List<Flow> lpf = new ArrayList<Flow>();
+			current_flow = flows.get(i); 
+			for(j = 0; j < flows.size(); j++){
+				compared_flow = flows.get(j);
+				if(current_flow.getPriority() == compared_flow.getPriority()){
+					if(j != i) spf.add(compared_flow);
+				}
+				else if(current_flow.getPriority() > compared_flow.getPriority()) lpf.add(compared_flow);
+				else if(current_flow.getPriority() < compared_flow.getPriority()) hpf.add(compared_flow);
+			}
+			current_flow.setHigherPriorityFlows(hpf);
+			current_flow.setLowerPriorityFlows(lpf);
+			current_flow.setSamePriorityFlows(spf);
+		}
+	}
+	
+	public void addFlow(Flow flow) {
+		flows.add(flow);
+	}
+	
 	public List<Flow> getFlows() {
 		return flows;
 	}
+	
 	public void setFlows(List<Flow> flows) {
 		this.flows = flows;
 	}
+	
 	public List<Node> getNodes() {
 		return nodes;
 	}
+	
 	public void setNodes(List<Node> nodes) {
 		this.nodes = nodes;
 	}
+	
 	public int getLmax() {
 		return lmax;
 	}
+	
 	public void setLmax(int lmax) {
 		this.lmax = lmax;
 	}
+	
 	public int getLmin() {
 		return lmin;
 	}
+	
 	public void setLmin(int lmin) {
 		this.lmin = lmin;
 	}
