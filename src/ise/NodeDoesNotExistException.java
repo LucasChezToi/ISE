0eff74f39a43d33eec21ba4be026a512ef8645b6
Raphael <ralfdu31@hotmail.fr>
2014-03-04 11:59:42 +0100

    Modification des classes d exception

--- src/ise/NodeDoesNotExistException.java
+++ src/ise/NodeDoesNotExistException.java
@@ -1,11 +1,13 @@
 package ise;
 
 public class NodeDoesNotExistException extends Exception {
-
+	public String message;
 	/**
 	 * generated ID
 	 */
 	private static final long serialVersionUID = 6905916248289513991L;
 	
-
+	public NodeDoesNotExistException(String message) {
+		this.message = message;
+	}
 }
