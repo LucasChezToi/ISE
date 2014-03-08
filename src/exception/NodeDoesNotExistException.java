c2209e2e1926bd27c9549b619910da888a410454
Raphael <ralfdu31@hotmail.fr>
2014-03-08 14:40:18 +0100

    packake exception ajouté

---
+++ src/exception/NodeDoesNotExistException.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class NodeDoesNotExistException extends Exception {
+	public String message;
+	/**
+	 * generated ID
+	 */
+	private static final long serialVersionUID = 6905916248289513991L;
+	
+	public NodeDoesNotExistException(String message) {
+		this.message = message;
+	}
+}
